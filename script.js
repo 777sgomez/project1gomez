12207dcf3fc550b256ee19d1f379094d6486f418
Susan Gomez <susangomez@Susans-MBP.attlocal.net>
2023-06-28 12:54:31 -0400

    Added a second userChoice2 and added a prompt for it that asks about rock paper or scissors along with a computerChoice to randomly select a number between 0-2 to script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 let userChoice = prompt("How many rounds do you want to play?").toLowerCase();
+
+let userChoice2 = prompt("Do you pick rock, paper, or scissors? (enter r, p, or s)");
+let computerChoice = Math.floor(Math.random() *3);
