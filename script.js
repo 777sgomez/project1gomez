f5acc1b76a148091e2f39bb9db9862b3064599d1
Susan Gomez <susangomez@Susans-MBP.attlocal.net>
2023-06-28 12:56:24 -0400

    Added if statements in regards to if the computer picked a number between the 0-2 there would be options for it to either be r,p, or s to play the game in script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ let userChoice = prompt("How many rounds do you want to play?").toLowerCase();
 
 let userChoice2 = prompt("Do you pick rock, paper, or scissors? (enter r, p, or s)");
 let computerChoice = Math.floor(Math.random() *3);
+
+if (computerChoice < 0.34)
+{
+    computerChoice = "r";
+}
+else if(computerChoice <= 1.67)
+{
+    computerChoice = "p";
+}
+else {
+    computerChoice = "s";
+}
