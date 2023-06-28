e44ba4df495b0603cf6316bce0d38013a58339b5
Susan Gomez <susangomez@Susans-MBP.attlocal.net>
2023-06-28 13:01:31 -0400

    Added a function to compare the two choices of userChoice2 and computerChoice. I made if statements regarding whether or not it would be a tie between the computer and the player and to alert the tie. I made an else if statement to make different alternative situations if the user picked r and the computer picked s and the alert to go with that. Lastly, I added a else if statement for computer to pick p and how to user would lose in script.js

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ else if(computerChoice <= 1.67)
 else {
     computerChoice = "s";
 }
+
+let compare = function (userChoice2, computerChoice)
+{
+    if (userChoice2 === computerChoice)
+    {
+        alert("The result is a tie!");
+    }
+    else if (userChoice2 === "r")
+    {
+        if(computerChoice === "s")
+        {
+            alert("You win! You picked " + userChoice2 + " which defeats " + computerChoice);
+        }
+        else if (computerChoice === "p")
+        {
+            alert("You lose! The computer picked " + computerChoice + " which defeats " + userChoice2);
+        }
+    }
+}
